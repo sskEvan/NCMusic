4d24676cd12dd4d6e055522cdffe37aa033a5ff6
sskEvan <642133969@qq.com>
2022-06-08 20:13:10 +0800

    修改BASE_URL

--- app/src/main/java/com/ssk/ncmusic/core/AppConfig.kt
+++ app/src/main/java/com/ssk/ncmusic/core/AppConfig.kt
@@ -4,6 +4,6 @@ package com.ssk.ncmusic.core
  * Created by ssk on 2022/4/17.
  */
 object AppConfig {
-    const val BASE_URL = "https://netease-cloud-music-api-eight-black.vercel.app/"
+    const val BASE_URL = "https://netease-cloud-music-api-fboox0on8-sskevan.vercel.app/"
     const val APP_DESIGN_WIDTH = 750
 }
